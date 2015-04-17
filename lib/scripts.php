ec6c7953703b787e11c20d05b569c8b476e80514
Peith Vergil <peith.vergil@gmail.com>
2015-04-17 15:03:11 +0800

    Register the Hopscotch JS library.

--- lib/scripts.php
+++ lib/scripts.php
@@ -1,39 +1,57 @@
 <?php namespace Ajul;
 
 /**
- * Register CSS files.
+ * Register admin CSS files.
+ *
+ * @since 0.1
+ */
+function register_admin_styles() {
+    wp_register_style('ajul-edit', AJUL_PLUGIN_URL . 'assets/css/ajul-edit.css', array('wp-jquery-ui-dialog'), AJUL_PLUGIN_VERSION);
+}
+
+add_action('admin_enqueue_scripts', 'Ajul\register_admin_styles');
+
+/**
+ * Register admin JS files.
+ *
+ * @since 0.1
+ */
+function register_admin_scripts() {
+    wp_register_script('ajul-common', AJUL_PLUGIN_URL . 'assets/js/ajul-common.js', array(
+        'jquery-ui-button',
+        'jquery-ui-dialog',
+        'backbone'
+    ), AJUL_PLUGIN_VERSION, true);
+
+    wp_register_script('ajul-edit', AJUL_PLUGIN_URL . 'assets/js/ajul-edit.js', array(
+        'ajul-common'
+    ), AJUL_PLUGIN_VERSION, true);
+}
+
+add_action('admin_enqueue_scripts', 'Ajul\register_admin_scripts');
+
+/**
+ * Register front-end CSS files.
  *
  * @since 0.1
  */
 function register_styles() {
-    $ajulEditDependencies = array(
-        'wp-jquery-ui-dialog'
-    );
+    wp_register_style('hopscotch', AJUL_PLUGIN_URL . 'assets/css/lib/hopscotch.css', array(), AJUL_PLUGIN_VERSION);
 
-    wp_register_style('ajul-edit', AJUL_PLUGIN_URL . 'assets/css/ajul-edit.css', $ajulEditDependencies, AJUL_PLUGIN_VERSION);
+    wp_register_style('ajul-tour', AJUL_PLUGIN_URL . 'assets/css/ajul-tour.css', array('hopscotch'), AJUL_PLUGIN_VERSION);
 }
 
-add_action('admin_enqueue_scripts', 'Ajul\register_styles');
+add_action('wp_enqueue_scripts', 'Ajul\register_styles');
 
 /**
- * Register JS files.
+ * Register front-end JS files.
  *
  * @since 0.1
  */
 function register_scripts() {
-    $ajulCommonDependencies = array(
-        'jquery-ui-button',
-        'jquery-ui-dialog',
-        'backbone'
-    );
+    wp_register_script('hopscotch', AJUL_PLUGIN_URL . 'assets/js/lib/hopscotch.js', array('jquery'), AJUL_PLUGIN_VERSION, true);
 
-    wp_register_script('ajul-common', AJUL_PLUGIN_URL . 'assets/js/ajul-common.js', $ajulCommonDependencies, AJUL_PLUGIN_VERSION, true);
-
-    $ajulEditDependencies = array(
-        'ajul-common'
-    );
-
-    wp_register_script('ajul-edit', AJUL_PLUGIN_URL . 'assets/js/ajul-edit.js', $ajulEditDependencies, AJUL_PLUGIN_VERSION, true);
+    wp_register_script('ajul-tour', AJUL_PLUGIN_URL . 'assets/js/ajul-tour.js', array('hopscotch'), AJUL_PLUGIN_VERSION, true);
 }
 
-add_action('admin_enqueue_scripts', 'Ajul\register_scripts');
+add_action('wp_enqueue_scripts', 'Ajul\register_scripts');
