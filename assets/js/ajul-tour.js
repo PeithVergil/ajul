48e20fc87f0440a9d3ea157043cc36e6ec872ddd
Peith Vergil <peith.vergil@gmail.com>
2015-04-18 15:19:58 +0800

    Allow the tour to be played automatically.

--- assets/js/ajul-tour.js
+++ assets/js/ajul-tour.js
@@ -1,11 +1,30 @@
 (function($, global) {
-    $(function() {
-        var tour = {
-            id: 'hello-hopscotch',
+    function AjulTour() {
+        this.tour = {
+            id   : AjulTourSettings.tourName,
             steps: AjulTourSettings.steps,
         };
+    }
 
+    AjulTour.prototype.start = function() {
         // Start the tour!
-        hopscotch.startTour(tour);
+        hopscotch.startTour(this.tour);
+    };
+
+    global.ajulTour = new AjulTour();
+}(jQuery, this));
+
+(function($, global) {
+    $(function() {console.log(AjulTourSettings);
+        if (AjulTourSettings.start) {
+            ajulTour.start();
+        } else {
+            $('#ajul-tour-start').click(function(e) {
+                e.preventDefault();
+
+                ajulTour.start();
+            });
+        }
+
     });
 }(jQuery, this));
